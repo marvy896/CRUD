7ee43c3a6c30133c12a2bc0699942f431e366213
marvy896 <onyex896@gmail.com>
2023-09-14 10:49:08 +0100

    update

--- Database/connect.js
+++ Database/connect.js
@@ -1,10 +1,14 @@
-import { connect } from 'mongoose';
+import mongoose from 'mongoose'
 
-const connectDB = async (url) => {
-    return connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-};
-
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://onyex896:<6xRl9LpBVXhDNezC>@cluster0.xvtr9pj.mongodb.net/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+}
 export default connectDB;
